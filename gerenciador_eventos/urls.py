2e4c59f33a04aa7cd9673692bc68de36f283cb01
vTerto <139240135+vTerto@users.noreply.github.com>
2025-02-22 13:57:09 -0300

    att urls.py

--- gerenciador_eventos/urls.py
+++ gerenciador_eventos/urls.py
@@ -1,7 +1,12 @@
 
 from django.urls import path, include
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Bem-vindo ao Sistema de Eventos!")
 
 urlpatterns = [
-    
+    path('', home, name='home'),
     path('api/', include('eventos.urls')),  # Inclui as rotas do app eventos
 ]
