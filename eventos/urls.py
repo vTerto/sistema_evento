0b05613d9be1d6f7e45a88a947a928cf27230f28
vTerto <139240135+vTerto@users.noreply.github.com>
2025-02-22 13:57:46 -0300

    att urls.py

--- eventos/urls.py
+++ eventos/urls.py
@@ -5,14 +5,16 @@ from .views import (
     deletar_evento, 
     RegistroUsuario, 
     LoginUsuario, 
-    inscrever_evento  # Nome correto aqui (antes estava InscreverEvento)
+    inscrever_evento,
+    home  
 )
 
 urlpatterns = [
+    path('', home, name='home'),  
     path('eventos/', listar_eventos, name='listar_eventos'),
     path('eventos/<int:pk>/', atualizar_evento, name='atualizar_evento'),
     path('eventos/<int:pk>/deletar/', deletar_evento, name='deletar_evento'),
     path('auth/registro/', RegistroUsuario.as_view(), name='registro_usuario'),
     path('auth/login/', LoginUsuario.as_view(), name='login_usuario'),
-    path('eventos/inscrever/', inscrever_evento, name='inscrever_evento'),  # Corrigido!
+    path('eventos/inscrever/', inscrever_evento, name='inscrever_evento'),  
 ]
